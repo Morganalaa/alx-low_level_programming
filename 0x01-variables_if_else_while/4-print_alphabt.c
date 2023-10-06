4e5333260b2d279cfd9ad082e2abf04a645c67f9
Morgan alaa <morganalaa78@gmail.com>
2023-10-06 12:33:45 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,9 @@ char ch = 'a';
 while (ch <= 'z')
 {
 if (ch != 'e' && ch != 'q')
-{														
-putchar(ch);													
-}										
+{										
+putchar(ch);									
+}
 ch++;										
 }
 putchar('\n');

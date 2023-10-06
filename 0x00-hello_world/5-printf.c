ca662e281aaa99cd44ef735e3ff2a789ad3a0daa
Morgan alaa <morganalaa78@gmail.com>
2023-10-05 18:05:34 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - using printf to print
- *   * Return: always 0
- *    */
+ *   main - using printf to print
+ *    Return: always 0
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");

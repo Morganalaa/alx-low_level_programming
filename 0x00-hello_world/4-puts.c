5601b84616fb7cc2555a5063548fd2b7fec1c132
Morgan alaa <morganalaa78@gmail.com>
2023-10-05 17:32:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-/* 
- * * main - display string using puts
- *   */
+/**
+ *  * main - display string using puts
+ *   * Return: always 0
+ *    */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");

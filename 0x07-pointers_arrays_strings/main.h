0c8355adb4bb9970ed642520e93099d0f712f523
Morgan alaa <morganalaa78@gmail.com>
2023-10-23 14:38:55 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@
  * used in the 0x06-pointers_arrays_strings directory.
  */
 
+#include <stddef.h>
+
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);

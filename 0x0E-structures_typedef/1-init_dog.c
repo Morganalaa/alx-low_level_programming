8501207b16ea3cdc90153414054f6e8dff87906b
Morgan alaa <morganalaa78@gmail.com>
2023-11-06 17:11:16 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,7 +6,6 @@
  *  @name: input for dog name
  *  @age: input for dog age
  *  @owner: input for dog owner
- *  Return: nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)

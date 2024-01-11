dd470ac7b93154e0c3631a6d854e07bf16955c02
Morgan alaa <morganalaa78@gmail.com>
2024-01-11 12:33:25 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
-	const dlistint_t *tmp = h;
-	while (tmp != NULL)
-	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-		i++;
-	}
-	return (i);
+size_t i = 0;
+const dlistint_t *tmp = h;
+while (tmp != NULL)
+{
+printf("%d\n", tmp->n);
+tmp = tmp->next;
+i++;
+}
+return (i);
 }

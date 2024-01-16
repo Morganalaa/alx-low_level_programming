591f907af28350386ea118741edfff2e8dc47418
Morgan alaa <morganalaa78@gmail.com>
2024-01-15 16:44:01 -0800

    t

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -4,13 +4,13 @@
  *_puts - swaps the value of two integers
  * @str: The pointer value one
  */
-void _puts(char *str)
+void _puts(char *s)
 {
 	int c = 0;
 
-	while (str[c] != '\0')
+	while (s[c] != '\0')
 	{
-		_putchar(str[c]);
+		_putchar(s[c]);
 		c++;
 	}
 	_putchar('\n');
